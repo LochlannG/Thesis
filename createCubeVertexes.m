75476f4af02e72834ac8d0cea316634f21aefdb4
User <eparesp@gmail.com>
2024-04-06 17:11:44 +0100

    Renamed the createCyclistVertexes.m function for clarity

--- createCubeVertexes.m
+++ createCubeVertexes.m
@@ -1,9 +1,9 @@
-function cyclist = createCylistVertexes(cyclist, xScale, yScale, zScale, rgb)
+function cube = createCubeVertexes(cube, xScale, yScale, zScale, rgb)
 
     
 
     % CoOrdinates of each vertex
-    cyclist.vertexCoords = single( [1, 1, 1;
+    cube.vertexCoords = single( [1, 1, 1;
                                     1, 1, -1;
                                     1, -1, -1;
                                     1, -1, 1;
@@ -12,16 +12,16 @@ function cyclist = createCylistVertexes(cyclist, xScale, yScale, zScale, rgb)
                                     -1, -1, -1;
                                     -1, -1, 1]);
 
-    nVerticies = length(cyclist.vertexCoords);
+    nVerticies = length(cube.vertexCoords);
 
     scalingArray = [ones(nVerticies, 1)*xScale, ones(nVerticies, 1)*yScale, ones(nVerticies, 1)*zScale];
-    cyclist.vertexCoords = cyclist.vertexCoords.*scalingArray;
+    cube.vertexCoords = cube.vertexCoords.*scalingArray;
     
     % Colour at each vertex
-    cyclist.vertexColors = single(ones(nVerticies, 3).*rgb); % colours at each vertex
+    cube.vertexColors = single(ones(nVerticies, 3).*rgb); % colours at each vertex
     
     % How the vertexes relate to each other (I wouldn't change this)
-    cyclist.elementArray = int32([0,1,2;
+    cube.elementArray = int32([0,1,2;
                                 3,0,3;
                                 7,4,0;
                                 4,5,1;
@@ -30,7 +30,7 @@ function cyclist = createCylistVertexes(cyclist, xScale, yScale, zScale, rgb)
                                 4,7,6;
                                 7,3,2]); % vertex numbers for the faces
     
-    cyclist.vertexCoords = reshape(cyclist.vertexCoords', 1, length(cyclist.vertexCoords)*3);
-    cyclist.vertexColors = reshape(cyclist.vertexColors', 1, length(cyclist.vertexColors)*3);
-    cyclist.elementArray = reshape(cyclist.elementArray', 1, length(cyclist.elementArray)*3);
+    cube.vertexCoords = reshape(cube.vertexCoords', 1, length(cube.vertexCoords)*3);
+    cube.vertexColors = reshape(cube.vertexColors', 1, length(cube.vertexColors)*3);
+    cube.elementArray = reshape(cube.elementArray', 1, length(cube.elementArray)*3);
 end
