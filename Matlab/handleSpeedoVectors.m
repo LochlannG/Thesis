4ec05ea0eb4a8ba7ea2217af8667205094071df8
User <User@137.43.148.180>
2024-04-12 14:36:58 +0100

    vector rotation maths

---
+++ Matlab/handleSpeedoVectors.m
@@ -0,0 +1,20 @@
+function [vector, newPos] = handleSpeedoVectors(camera, distance)
+
+    % 
+    % figure
+    % [a, newPos] = handleSpeedoVectors(camera, distance);
+    % 
+    % quiver3(camera.xyz(1), camera.xyz(2), camera.xyz(3), a(1, 1), a(1, 2), a(1, 3), 0)
+    % hold on
+    % axis equal
+    % plot3(camera.xyz(1), camera.xyz(2), camera.xyz(3), 'ro')
+    % plot3(camera.fixPoint(1), camera.fixPoint(2), camera.fixPoint(3), 'bo')
+    % plot3(newPos(1), newPos(2), newPos(3), 'go')
+
+    vector = camera.fixPoint - camera.xyz;
+    vector = (vector/norm(vector));
+    
+    newPos = vector*distance + camera.xyz;
+    
+end
+
